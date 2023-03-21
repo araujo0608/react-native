41b09dff52fcacc6b2b5d1380133460b0a020eae
araujo0608 <68195900+araujo0608@users.noreply.github.com>
2023-03-20 21:40:28 -0300

    upda

--- navigation/App.js
+++ navigation/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-//import { StyleSheet, Text, View } from 'react-native';
 import Contacts from './src/pages/Contacts/';
 import Informations from './src/pages/Informations/';
-//import styles from './style'
 
 
 const Stack = createNativeStackNavigator();
